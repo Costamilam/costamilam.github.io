e6377ab784a131053dd68e76acbf699cb92cd212
Guilherme Costamilam <costamilam073@gmail.com>
2019-08-26 18:08:35 -0300

    Notify when creating message

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
+const http = require('http');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
+const { serverKey, reciverKey } = require('./keys');
 
 admin.initializeApp();
 
@@ -8,7 +11,20 @@ module.exports.setPostDate = functions.database.ref('/messages/{id}').onCreate((
 
     data.date = new Date().toISOString();
 
-    snapshot.ref.set(data).catch(error => {
+    snapshot.ref.set(data).then(() => {
+        http.request({
+            host: 'fcm.googleapis.com',
+            path: '/fcm/send',
+            method: 'POST',
+            headers: {
+                'Authorization': `key=${serverKey}`,
+                'Content-Type': 'application/json'
+            }
+        }).end(JSON.stringify({
+            data: data,
+            to: reciverKey
+        }));
+    }).catch(error => {
         console.error('Failed to add date when creating message: ', error)
     });
 });
